d29a7f0fb709a85cbe3dadb61e873971f5067994
kuitan <kuitan812@gmail.com>
2018-12-08 07:23:05 +0000

    あ

--- enemy.rb
+++ enemy.rb
@@ -1,13 +1,18 @@
 # coding: utf-8
 
 class Enemy < Sprite
+  def initialize(x,y,img,speed)
+    super(x,y,img)
+    @speed = speed
+  end
   def update
-    self.y += 1
+    self.y += @speed
+    self.x += rand(8)-rand(8)
     if self.y >= Window.height - self.image.height
       self.vanish
     end
   end
-
+  
   # 他のオブジェクトから衝突された際に呼ばれるメソッド
   def hit
     self.vanish
