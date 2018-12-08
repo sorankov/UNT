654ea87015133bcde3ef9e192e01c1d06ab7472f
kuitan <kuitan812@gmail.com>
2018-12-08 08:30:27 +0000

    ボーンを追加

--- main.rb
+++ main.rb
@@ -4,9 +4,13 @@ include DXOpal
 
 require_remote 'player.rb'
 require_remote 'enemy.rb'
+require_remote 'bone.rb'
 
 Image.register(:player, 'images/player.png') 
-Image.register(:enemy, 'images/enemy1.png') 
+Image.register(:enemy, 'images/enemy1.png')
+Image.register(:bone, 'images/bone.png')
+
+bn_x = 0
 
 Window.load_resources do
   Window.width  = 800
@@ -19,6 +23,9 @@ Window.load_resources do
 
   enemy_img = Image[:enemy]
   enemy_img.set_color_key([0, 0, 0])
+  
+  bone_img = Image[:bone]
+  bone_img.set_color_key([0, 0, 0])
 
   player = Player.new(384, 300, player_img)
 
@@ -40,6 +47,14 @@ Window.load_resources do
   end
   30.times do
     enemies << Enemy.new(rand(800), rand(600)-4200, enemy_img, 5)
+  end
+  50.times do
+    enemies << Bone.new(bn_x + 1400, 432, bone_img, 0)
+    bn_x -= 25
+  end
+  50.times do
+    enemies << Bone.new(bn_x + 2400, 100, bone_img, 180)
+    bn_x -= 25
   end
   
   Window.loop do
