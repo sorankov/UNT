b705b72b45e7420d16dde9030d1e6b646974127a
Takao Fukushima <fsorankovt722@gmail.com>
2018-12-08 07:38:40 +0000

    Revert "あ"
    This reverts commit d29a7f0fb709a85cbe3dadb61e873971f5067994.

--- enemy.rb
+++ enemy.rb
@@ -1,18 +1,13 @@
 # coding: utf-8
 
 class Enemy < Sprite
-  def initialize(x,y,img,speed)
-    super(x,y,img)
-    @speed = speed
-  end
   def update
-    self.y += @speed
-    self.x += rand(8)-rand(8)
+    self.y += 1
     if self.y >= Window.height - self.image.height
       self.vanish
     end
   end
-  
+
   # 他のオブジェクトから衝突された際に呼ばれるメソッド
   def hit
     self.vanish
