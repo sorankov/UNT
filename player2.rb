3ba9cdfe1ce0bfb3f9988a1cd51e3e6c33232f4b
TomoyaMochida <world.world-@ezweb.ne.jp>
2018-12-08 08:47:09 +0000

    player2追加

--- player2.rb
+++ player2.rb
@@ -1,8 +1,22 @@
-class Player < Sprite
+class Player2 < Sprite
+
+  attr_accessor :jump, :dir
+  
+  def initialize
+    @jump = 0
+    @dir = 0
+    super
+ 
+  end
+
   def update
+    if Input.key_push?(K_UP)
+        @jump = 1
+        p "up"
+    end
     
     self.x += Input.x * 5
-    self.y = 100
+    
     
     if self.x < 200 
         self.x = 200
@@ -10,13 +24,21 @@ class Player < Sprite
         self.x = 568
     end
     
-    if ???
-        until self.y >= 300
-            self.y += 1
+    if @jump == 1
+        p "up2"
+        if @dir == 0 && self.y >= 250
+            self.y -= 1 * 6
+            if self.y < 250
+                @dir = 1
+            end
+        elsif @dir ==1 && self.y <= 460
+            self.y += 1 * 6
+            if self.y > 460
+                @dir = 0
+                @jump = 0
+            end
         end
-        until self.y <= 100
-            self.y -= 1
-        end
+        
     end
     
   end
